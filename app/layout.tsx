e0453b8aff057adac6e2ea7582af44ad639bc665
Aineah Marabi <aineahbwire@gmail.com>
2024-12-08 11:36:34 +0300

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,16 +20,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body 
+        className="antialiased"
+        suppressHydrationWarning={true}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
